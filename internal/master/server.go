aaa8bb04f4ceb08c1b6db25664bf093b9020e8cf
Ryotaro Kotaka <225878868+Ryo-11GRX@users.noreply.github.com>
2026-02-17 19:56:18 +0900

    feat(master): add StartGRPCServer function to initialize gRPC server
    - Implement StartGRPCServer to listen on a specified port
    - Register SwarmService with the gRPC server
    - Log server startup information

--- internal/master/server.go
+++ internal/master/server.go
@@ -1,6 +1,12 @@
 package master
 
 import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
 	"github.com/ryokotaka/SwarmGo/proto"
 )
 
@@ -12,4 +18,15 @@ type Server struct {
 // NewServer は新しい Server を返す。
 func NewServer() *Server {
 	return &Server{}
+}
+
+func StartGRPCServer(port string) error {
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	proto.RegisterSwarmServiceServer(s, NewServer())
+	log.Printf("Master server listening on port %s...", port)
+	return s.Serve(lis)
 }
