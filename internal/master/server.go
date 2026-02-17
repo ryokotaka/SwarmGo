5ef4a07b10d70bbc7329f5c3eeb99f6d84b7178a
Ryotaro Kotaka <225878868+Ryo-11GRX@users.noreply.github.com>
2026-02-17 20:20:32 +0900

    feat(master): implement Connect method for bidirectional streaming
    - Add Connect method to handle incoming streams from Workers
    - Implement message reception and logging for Worker connections

--- internal/master/server.go
+++ internal/master/server.go
@@ -2,6 +2,7 @@ package master
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -15,11 +16,39 @@ type Server struct {
 	proto.UnimplementedSwarmServiceServer
 }
 
-// NewServer は新しい Server を返す。
+// サーバーの初期化
 func NewServer() *Server {
 	return &Server{}
 }
 
+// Connect は Worker からの双方向ストリームを処理する。
+func (s *Server) Connect(stream proto.SwarmService_ConnectServer) error {
+	// 1. 最初のメッセージを受信 (Register)
+	// Recv() の内部で、ワイヤ上の「タグ番号＋値」が .proto の定義に従って WorkerMsg に復元される
+	msg, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+	reg := msg.GetRegister()
+	if reg == nil {
+		return fmt.Errorf("first message must be Register")
+	}
+	log.Printf("Worker connected: %s (Arch: %s)", reg.WorkerId, reg.CpuArch)
+
+    // 4. 接続維持ループ
+	for {
+        // 受信: バイト列がタグ番号（1=Register, 2=Stats, 3=Finish）で解釈され、msg に復元されている
+		_, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+	}
+}
+
+// サーバーの起動
 func StartGRPCServer(port string) error {
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
