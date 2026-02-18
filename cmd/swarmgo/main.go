a2e4b4e7a8d7f19f6fb60f95a3bc61be3fec4b76
Ryotaro Kotaka <225878868+Ryo-11GRX@users.noreply.github.com>
2026-02-18 22:13:40 +0900

    feat(main): enhance command handling and add help functionality
    - Implement command parsing for "master" and "worker" modes
    - Add printHelp function to display usage instructions and options
    - Introduce runMaster and runWorker functions for starting respective modes
    - Improve error handling for unknown commands and connection issues

--- cmd/swarmgo/main.go
+++ cmd/swarmgo/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/ryokotaka/SwarmGo/internal/master"
+	"github.com/ryokotaka/SwarmGo/internal/worker"
 )
 
 func main() {
@@ -23,17 +24,48 @@ func main() {
         cmd = "worker"
     }
 
-    switch cmd {
-    case "master":
-        // Masterモードのフラグ解析と実行
-        runMaster()
-    case "worker":
-        // Workerモードのフラグ解析と実行
-        runWorker()
-    default:
+	switch cmd {
+	case "master":
+		runMaster()
+	case "worker":
+		runWorker()
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", os.Args[1])
+		printHelp()
+		os.Exit(1)
+	}
+}
 
-        fmt.Printf("Unknown command: %s\n", os.Args[1])
-        printHelp()
-        os.Exit(1)
-    }
+func printHelp() {
+	fmt.Fprintln(os.Stderr, "usage: swarmgo <master|worker> [options]")
+	fmt.Fprintln(os.Stderr, "  master  - start the Master gRPC server")
+	fmt.Fprintln(os.Stderr, "  worker  - connect to Master and run load test tasks")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "master options:")
+	flag.PrintDefaults()
+}
+
+func runMaster() {
+	port := flag.String("p", "50051", "gRPC listen port")
+	flag.CommandLine.Parse(os.Args[2:])
+	if err := master.StartGRPCServer(*port); err != nil {
+		fmt.Fprintf(os.Stderr, "master: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func runWorker() {
+	addr := os.Getenv("MASTER_ADDR")
+	if addr == "" {
+		addr = "localhost:50051"
+	}
+	client, err := worker.NewGRPCClient(addr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "worker: %v\n", err)
+		os.Exit(1)
+	}
+	if err := client.Start(); err != nil {
+		fmt.Fprintf(os.Stderr, "worker: %v\n", err)
+		os.Exit(1)
+	}
 }
